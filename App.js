60dd57cdcbfb76f9baeaa033f6d6955fc1e03002
SriwanKm <kcqkoo@gmail.com>
2021-04-02 14:09:14 -0700

    add header, navBar, sections

--- App.js
+++ App.js
@@ -1,13 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React from "react";
 import {
   SafeAreaView,
   ScrollView,
@@ -16,97 +7,135 @@ import {
   Text,
   useColorScheme,
   View,
-} from 'react-native';
+} from "react-native";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={styles.borderContainer}>
+          <View style={styles.headerContainer}>
+            <Text style={styles.mainHeader}>Green Creative</Text>
+            <Text style={styles.subHeader}>
+              Inspired by Creative Commons, licensed under Creative Commons.
+            </Text>
+          </View>
+          <ScrollView>
+            <View style={styles.navBar}>
+              <Text style={styles.navText}>home</Text>
+              <Text style={styles.navText}>about</Text>
+              <Text style={styles.navText}>portfolio</Text>
+              <Text style={styles.navText}>prices</Text>
+              <Text style={styles.navText}>products</Text>
+              <Text style={styles.navText}>faq</Text>
+              <Text style={styles.navText}>contact</Text>
+            </View>
+          </ScrollView>
+          <View style={styles.section}>
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionHeaderText}>Template Usage</Text>
+            </View>
+            <View style={styles.sectionDetails}>
+              <Text style={styles.sectionDetailsText}>
+                You may use this template on any site, anywhere, for free just please leave the link back to me in the
+                footer. This template validates XHTML Strict 1.0, CSS Validates as well; enjoy :)
+              </Text>
+              <Text style={styles.sectionDetailsText}>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer mi. Vivamus sit amet neque vitae
+                sapien bibendum sodales. Curabitur elementum. Duis imperdiet. Donec eleifend porttitor sapien. Praesent
+                leo. Quisque auctor velit sed tellus. Suspendisse potenti. Aenean laoreet imperdiet nunc. Donec commodo
+                suscipit dolor. Aenean nibh. Sed id odio. Aliquam lobortis risus ut felis. Sed vehicula pellentesque
+                quam.x
+              </Text>
+            </View>
+          </View>
+
+          <View style={styles.section}>
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionHeaderText}>Another Title Goes Here!</Text>
+            </View>
+            <View style={styles.sectionDetails}>
+              <Text style={styles.sectionDetailsText}>
+                This particular template goes not have a naviagation panel; it was intended for simple sites. I am also
+                making a version of this template with navigation, check out BryantSmith.com to get it.
+              </Text>
+            </View>
+          </View>
+
+          <View style={styles.section}>
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionHeaderText}>Yet Another?</Text>
+            </View>
+            <View style={styles.sectionDetails}>
+              <Text style={styles.sectionDetailsText}>
+                Each title is an H1 tag, so choose them carefully :)
+              </Text>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: "#67915a",
+    padding: 20,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  borderContainer: {
+    flex: 1,
+    borderColor: "#bfd3b7",
+    borderWidth: 8,
+    borderRadius: 10,
+    backgroundColor: "blue",
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  headerContainer: {
+    backgroundColor: "#dcb93e",
+    padding: 10,
   },
-  highlight: {
-    fontWeight: '700',
+  mainHeader: {
+    color: "#293823",
+    fontSize: 28,
+    fontWeight: "bold",
+  },
+  subHeader: {
+    color: "#67915a",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  navBar: {
+    backgroundColor: "#836b18",
+    padding: 2,
+    paddingHorizontal: 10,
+    flexDirection: "row",
+  },
+  navText: {
+    color: "#fafcf9",
+    padding: 15,
+    fontWeight: "bold",
+  },
+  section: {
+    backgroundColor: "#ffffff",
+    padding: 14,
+  },
+  sectionHeader: {
+    backgroundColor: "#ebf1ea",
+    padding: 10,
+    borderRadius: 30,
+  },
+  sectionDetails: {
+    backgroundColor: "#ffffff",
+  },
+  sectionHeaderText: {
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+  sectionDetailsText: {
+    fontSize: 15,
+    lineHeight: 28,
+    marginBottom: 18,
   },
 });
-
 export default App;
